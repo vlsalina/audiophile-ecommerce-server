5dfe0c00531dbd78e3dddc785c4302ae3d33278a
vlsalina <vlsalina@ucsd.edu>
2022-02-15 20:30:09 -0800

    Configured CORS policy headers.

--- app.js
+++ app.js
@@ -36,6 +36,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
