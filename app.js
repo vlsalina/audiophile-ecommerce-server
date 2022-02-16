fc2d37350f4ea2ce1fbf2d604774105bf8ba6f4c
vlsalina <vlsalina@ucsd.edu>
2022-02-16 02:46:03 -0800

    Configured cors headers.

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
   next();
 });
 
