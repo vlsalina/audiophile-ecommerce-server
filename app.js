ba426618653613f62240393dd047532bb6617610
vlsalina <vlsalina@ucsd.edu>
2022-03-18 23:07:12 -0700

    Configure allowed origins to heroku url.

--- app.js
+++ app.js
@@ -37,7 +37,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://audiophile-ecommerce-app.netlify.app"
+  );
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
   next();
